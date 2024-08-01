c3dbf57cc096f601e80a8a2c09dfcf1d0763ae75
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-01 16:25:49 -0300

    Aula: Estrutura de repetição: While e Do While

--- Curso-em-video/curso - javascript/aula15-EstruturaDeRepeticao1/script.js
+++ Curso-em-video/curso - javascript/aula15-EstruturaDeRepeticao1/script.js
@@ -2,8 +2,16 @@
 //!Estrutura de repetição
 
 var c = 1
-
-while (c <= 6) {
-    console.log("Tudo bem ?")
+do {
+    console.log(`Passo ${c}`)
     c++
-}
+} while (c <= 6)
+
+
+
+// var c = 1
+
+// while (c <= 6) {
+//     console.log("Tudo bem ?")
+//     c++
+// }
