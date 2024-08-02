08228eb7a27c432e88322d6ac7cd16e574a25787
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-02 12:02:21 -0300

    Aula: Exercicio de Estruturas de Repetição - Tabuada

--- Curso-em-video/curso - javascript/aula18-Exercicios-Tabuada/script.js
+++ Curso-em-video/curso - javascript/aula18-Exercicios-Tabuada/script.js
@@ -1,22 +1,31 @@
 
 // ! Exercicios Tabuada
 
-function tabuadar() {
+function tabuada() {
     let num = document.getElementById('txtn');
     let tab = document.getElementById('seltab');
 
     if (num.value.length === 0) {
 
-        window.alert('Por favor, digite um número!')
+        window.alert('Por favor, digite um número!');
 
     } else {
         
         let n = Number(num.value);
         let c = 1
+        tab.innerHTML = '' //Para limpar o campo antes de mostrar a tabuada
+
         while (c <= 10) {
-            let item = document.createElement('option')
+
+            let item = document.createElement('option');
+
+            item.text = `${n} x ${c} = ${n*c}`
+            // item.style.width = '150px'
+            item.value = `tab${c}`
+
+            tab.appendChild(item) //appendChild = adicionar elemento filho
+            c++
         }
     }
-
 
 }
