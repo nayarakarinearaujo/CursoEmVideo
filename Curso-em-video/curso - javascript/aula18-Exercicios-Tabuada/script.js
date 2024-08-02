444bb2ca2b6c7a9f240a2db35d15e83523511125
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-02 11:40:51 -0300

    Aula: Exercicio tabuada: Ajustes HTML

--- Curso-em-video/curso - javascript/aula18-Exercicios-Tabuada/script.js
+++ Curso-em-video/curso - javascript/aula18-Exercicios-Tabuada/script.js
@@ -1,7 +1,22 @@
 
-// ! Exercicios
+// ! Exercicios Tabuada
 
-function calcular() {
-   
+function tabuadar() {
+    let num = document.getElementById('txtn');
+    let tab = document.getElementById('seltab');
+
+    if (num.value.length === 0) {
+
+        window.alert('Por favor, digite um número!')
+
+    } else {
+        
+        let n = Number(num.value);
+        let c = 1
+        while (c <= 10) {
+            let item = document.createElement('option')
+        }
+    }
+
 
 }
