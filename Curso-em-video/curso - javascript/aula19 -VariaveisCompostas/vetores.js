4c6adf859a5b9538c60585ca9ce40f8fc18b0e31
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-06 10:05:20 -0300

    Aula: Buscando Vetores

--- Curso-em-video/curso - javascript/aula19 -VariaveisCompostas/vetores.js
+++ Curso-em-video/curso - javascript/aula19 -VariaveisCompostas/vetores.js
@@ -32,3 +32,4 @@ let valores1 = [8, 1, 7, 4, 2, 9, 13];
 for (let pos in valores1) {
     console.log(`A posição ${pos} tem o valor: ${valores1[pos]}`);
 }
+
---
+++ Curso-em-video/curso - javascript/aula19 -VariaveisCompostas/vetores2.js
@@ -0,0 +1,9 @@
+
+//* Buscando valor dentro do array
+
+let num = [5, 8, 2, 9, 3];
+let pos = num.indexOf(4);
+if(pos == -1) {
+    console.log("O valor não foi encontrado :( ")
+}
+console.log(`O valor 8 está na posição: ${pos}`);
